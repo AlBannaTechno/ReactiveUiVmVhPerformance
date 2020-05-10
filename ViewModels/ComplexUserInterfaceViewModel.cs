0ab36926162e33f7ba3b3e03a011ee545f49aac4
OsamaAlBanna <Al_Banna_Techno@yahoo.com>
2020-05-10 17:23:22 +0200

    Modify ComplexUI

--- ViewModels/ComplexUserInterfaceViewModel.cs
+++ ViewModels/ComplexUserInterfaceViewModel.cs
@@ -4,10 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace ReactiveUiVmVhPerformance.ViewModels
 {
     public class ComplexUserInterfaceViewModel : ReactiveObject
     {
+        [Reactive] public string FirstName {get; set;}
+        [Reactive] public string LastName {get; set;}
+        [Reactive] public string Email {get; set;}
+
+        [Reactive] public string FirstName1 {get; set;}
+        [Reactive] public string LastName2 {get; set;}
+        [Reactive] public string Email3 {get; set;}
+
+        [Reactive] public string FirstName4 {get; set;}
+        [Reactive] public string LastName5 {get; set;}
+        [Reactive] public string Email6 {get; set;}
+
+        [Reactive] public string FirstName7 {get; set;}
+        [Reactive] public string LastName8 {get; set;}
+        [Reactive] public string Email9 {get; set;}
+
+        [Reactive] public string FirstName10 {get; set;}
+        [Reactive] public string LastName11 {get; set;}
+        [Reactive] public string Email12 {get; set;}
+
+
+        [Reactive] public string FirstName13 {get; set;}
+        [Reactive] public string LastName14 {get; set;}
+        [Reactive] public string Email15 {get; set;}
+
+
+        [Reactive] public string FirstName16 {get; set;}
+        [Reactive] public string LastName17 {get; set;}
+        [Reactive] public string Email18 {get; set;}
+
+
     }
 }
--- Views/ComplexUserInterfaceView.xaml.cs
+++ Views/ComplexUserInterfaceView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,40 @@ namespace ReactiveUiVmVhPerformance.Views
         public ComplexUserInterfaceView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel, vm => vm.FirstName, v => v.FirstName.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName, v => v.LastName.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email, v => v.Email.Text).DisposeWith(disposable);
+
+
+                this.Bind(ViewModel, vm => vm.FirstName1, v => v.FirstName1.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName2, v => v.LastName2.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email3, v => v.Email3.Text).DisposeWith(disposable);
+
+                this.Bind(ViewModel, vm => vm.FirstName4, v => v.FirstName4.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName5, v => v.LastName5.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email6, v => v.Email6.Text).DisposeWith(disposable);
+
+                this.Bind(ViewModel, vm => vm.FirstName7, v => v.FirstName7.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName8, v => v.LastName8.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email9, v => v.Email9.Text).DisposeWith(disposable);
+
+                this.Bind(ViewModel, vm => vm.FirstName10, v => v.FirstName10.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName11, v => v.LastName11.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email12, v => v.Email12.Text).DisposeWith(disposable);
+
+                this.Bind(ViewModel, vm => vm.FirstName13, v => v.FirstName13.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName14, v => v.LastName14.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email15, v => v.Email15.Text).DisposeWith(disposable);
+
+                this.Bind(ViewModel, vm => vm.FirstName16, v => v.FirstName16.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.LastName17, v => v.LastName17.Text).DisposeWith(disposable);
+                this.Bind(ViewModel, vm => vm.Email18, v => v.Email18.Text).DisposeWith(disposable);
+
+
+            });
         }
     }
 }
